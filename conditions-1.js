4f04e763daa8fad278f88b7bf7af703c5d1cf82c
Zarya <rubka102@gmail.com>
2023-06-06 12:24:14 +0300

    Разделение скриптов

--- conditions-1.js
+++ conditions-1.js
@@ -1,4 +1,5 @@
 
+/* 1 задача */
 document.getElementById("checkButton").addEventListener("click", function() {
     let number = document.getElementById("number-1").value;
     if (number < 1 || number > 7) {
@@ -7,34 +8,3 @@ document.getElementById("checkButton").addEventListener("click", function() {
     let days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота","Воскресенье"];
     document.getElementById("result-1").innerHTML = "День недели: " + days[number - 1];}
 });
-
-
-/* 3 задача */
-let numberInput = document.getElementById('number-3');
-let checkButton = document.getElementById('checkButton-3');
-let result = document.getElementById('result-3');
-
-checkButton.addEventListener('click', function() {
-    let number = numberInput.value;
-
-    if (number.length !== 4 || isNaN(number)) {
-        result.textContent = 'Введите четырехзначное число!';
-    } else {
-        let leftHalf = number.slice(0, 2);
-        let rightHalf = number.slice(2);
-
-        let leftSum = 0;
-        let rightSum = 0;
-
-        for (let i = 0; i < leftHalf.length; i++) {
-            leftSum += parseInt(leftHalf[i]);
-            rightSum += parseInt(rightHalf[i]);
-        }
-
-        if (leftSum === rightSum) {
-            result.textContent = 'Это счастливый билет, поздравляю!';
-        } else {
-            result.textContent = 'Это не счастливый билет, потому что ты - лох :(';
-        }
-    }
-})
