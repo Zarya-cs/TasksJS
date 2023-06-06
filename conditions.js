8d3a59eeebf1bb9561d906bd929d3e1f117a3b48
Zarya <rubka102@gmail.com>
2023-06-06 10:58:22 +0300

    3 задача

--- conditions.js
+++ conditions.js
@@ -8,7 +8,7 @@ document.getElementById("checkButton").addEventListener("click", function() {
     document.getElementById("result-1").innerHTML = "День недели: " + days[number - 1];}
 });
 
-
+/* 2 задача */
 function checkPalindrome() {
     let number = document.getElementById("number-2").value;
     if (number.length != 4) {
@@ -21,3 +21,32 @@ function checkPalindrome() {
         document.getElementById("result-2").innerHTML = "Число не является палиндромом";
     }
 }
+/* 3 задача */
+let numberInput = document.getElementById('number-3');
+let checkButton = document.getElementById('checkButton-3');
+let result = document.getElementById('result-3');
+
+checkButton.addEventListener('click', function() {
+    let number = numberInput.value;
+
+    if (number.length !== 4 || isNaN(number)) {
+        result.textContent = 'Введите четырехзначное число!';
+    } else {
+        let leftHalf = number.slice(0, 2);
+        let rightHalf = number.slice(2);
+
+        let leftSum = 0;
+        let rightSum = 0;
+
+        for (let i = 0; i < leftHalf.length; i++) {
+            leftSum += parseInt(leftHalf[i]);
+            rightSum += parseInt(rightHalf[i]);
+        }
+
+        if (leftSum === rightSum) {
+            result.textContent = 'Это счастливый билет, поздравляю!';
+        } else {
+            result.textContent = 'Это не счастливый билет, потому что ты - лох :(';
+        }
+    }
+})
