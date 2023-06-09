7b0085ba12986bce3f64d22b0e147d3b261ad789
Zarya <rubka102@gmail.com>
2023-06-09 11:44:29 +0300

    1 задача по одномерным массивам

--- ODArrays-1.js
+++ ODArrays-1.js
@@ -1,5 +1,21 @@
-function getRandomNumber(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min) + min)
+function generateRandomSequence(n) {
+    let sequence = [];
+    for (let i = 0; i < n; i++) {
+        sequence.push(Math.floor(Math.random() * 57) - 23);
+    }
+    return sequence;
 }
+
+document.getElementById('generate').addEventListener('click', function(event) {
+    event.preventDefault()
+    let n = parseInt(document.getElementById('n').value);
+    let result = document.getElementById('result-gen');
+
+    if (isNaN(n) || n <= 0) {
+        result.innerText = 'Введите корректное количество элементов';
+        return;
+    }
+
+    let sequence = generateRandomSequence(n);
+    result.innerText = 'Последовательность: ' + sequence.join(', ');
+})
